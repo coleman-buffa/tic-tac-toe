b4e95632bcb36aae975973fb81aa92101725a5c8
Coleman Buffa <coleman.buffa@gmail.com>
2021-06-15 12:50:28 -0700

    Add functionality to accept input and mark the board accordingly. Input validation implemented.

--- index.js
+++ index.js
@@ -25,11 +25,39 @@ function markBoard(position, letter) {
 
 function printBoard() {
   console.log('\n' +
-      ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + '\n' +
-      ' ---------\n' +
-      ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + '\n' +
-      ' ---------\n' +
-      ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n');
+    ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + '\n' +
+    ' ---------\n' +
+    ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + '\n' +
+    ' ---------\n' +
+    ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n');
 }
 
-printBoard();
+function takeTurn(player) {
+  inquirer.prompt([
+    {
+      name: 'playerMove',
+      type: 'number',
+      message: 'Player ' + player + ': enter a number 1-9',
+      validate: function (value) {
+        if (
+          value === 1 || value === 2 || value === 3 ||
+          value === 4 || value === 5 || value === 6 ||
+          value === 7 || value === 8 || value === 9) {
+          return true
+        } else {
+          return 'Enter a number 1-9'
+        }
+      }
+    }
+  ]).then(function (response) {
+    markBoard(response.playerMove, player);
+
+    //Logic to check for win or draw goes here otherwise the next player takes their turn
+
+    printBoard();
+  })
+
+
+}
+
+takeTurn('X');
